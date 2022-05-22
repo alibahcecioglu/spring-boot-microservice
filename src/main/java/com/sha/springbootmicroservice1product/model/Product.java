1c058eebc16e5389cfecd04a23c3cd93aca24716
alibahcecioglu <alibahcecioglu9@gmail.com>
2022-05-22 19:39:01 +0300

    product_19;30_1

--- src/main/java/com/sha/springbootmicroservice1product/model/Product.java
+++ src/main/java/com/sha/springbootmicroservice1product/model/Product.java
@@ -14,6 +14,7 @@ public class Product
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(name = "name", length = 100, nullable = false)
     private String name;
 
